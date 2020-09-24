a7b8e315967d64d6bf3e9b3432f889c07b126508
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2020-09-24 12:20:26 +0530

    Initial Commit with dummy data

---
+++ Library.Api/Library.Api/Controllers/BookController.cs
@@ -0,0 +1,94 @@
+﻿using Library.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Library.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        public List<BookModel> Books { get; set; }
+
+        public BookController()
+        {
+            Books = new List<BookModel>();
+
+            Books.Add(new BookModel { ID = 1, Name = "Clean Architecture", Genre = "Programming", Author = "Robert Martin", PublishedYear = "2017" });
+            Books.Add(new BookModel { ID = 2, Name = "Dependency Injection in .NET", Genre = "Programming", Author = "Mark Seemann", PublishedYear = "2019" });
+            Books.Add(new BookModel { ID = 3, Name = "Refactoring", Genre = "Programming", Author = "Martin Fowler", PublishedYear = "1999" });
+            Books.Add(new BookModel { ID = 4, Name = "India After Gandhi", Genre = "History", Author = "Ramachandra Guha", PublishedYear = "2007" });
+            Books.Add(new BookModel { ID = 5, Name = "Gitanjali", Genre = "Poetry", Author = "Rabindranath Tagore", PublishedYear = "1910" });
+        }
+
+        // GET: api/<BookController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Books);
+        }
+
+        // GET api/<BookController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var book = Books.Where(i => i.ID == id).FirstOrDefault();
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        // POST api/<BookController>
+        [HttpPost]
+        public IActionResult Post([FromBody] BookModel book)
+        {
+            if (book == null)
+            {
+                return BadRequest();
+            }
+
+            Books.Add(book);
+            return Ok(book);
+        }
+
+        // PUT api/<BookController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] BookModel book)
+        {
+            if (book == null)
+            {
+                return BadRequest();
+            }
+
+            var existingBook = Books.Where(i => i.ID == book.ID).FirstOrDefault();
+            if (existingBook == null)
+            {
+                return NotFound();
+            }
+            Books.Remove(existingBook);
+            Books.Add(book);
+
+            return Ok(existingBook);
+        }
+
+        // DELETE api/<BookController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+
+            var existingBook = Books.Where(i => i.ID == id).FirstOrDefault();
+            if (existingBook == null)
+            {
+                return NotFound();
+            }
+            Books.Remove(existingBook);
+
+            return Ok();
+        }
+    }
+}
---
+++ Library.Api/Library.Api/Models/BookModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Library.Api.Models
+{
+    public class BookModel
+    {
+        public BookModel()
+        {
+        }
+
+        public long ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Author { get; set; }
+
+        public string PublishedYear { get; set; }
+    }
+}
